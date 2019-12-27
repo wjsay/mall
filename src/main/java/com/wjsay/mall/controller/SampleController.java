3f1287e1b583afb7cea9a76b15fdba2d235db30a
13523963931 <472331573@qq.com>
2019-12-27 17:51:07 +0800

    create mal

---
+++ src/main/java/com/wjsay/mall/controller/SampleController.java
@@ -0,0 +1,15 @@
+package com.wjsay.mall.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/demo")
+public class SampleController {
+    @RequestMapping("/thymeleaf")
+    public String thymeleaf(Model model) {
+        model.addAttribute("name", "wjsay");
+        return "hello";
+    }
+}
