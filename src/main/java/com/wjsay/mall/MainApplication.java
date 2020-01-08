730471e696a1393c1295db3196049da02572d81a
zzuwenjie <wen20121724@163.com>
2020-01-08 13:43:34 +0800

    add docker

--- src/main/java/com/wjsay/mall/MainApplication.java
+++ src/main/java/com/wjsay/mall/MainApplication.java
@@ -2,9 +2,17 @@ package com.wjsay.mall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MainApplication {
+public class MainApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MainApplication.class);
+    }
+
     public static void main(String[] args) throws Exception {
         SpringApplication.run(MainApplication.class, args);
     }
