3ff40181791467229d17ac52700941da723f2296
zzuwenjie <wen20121724@163.com>
2021-02-19 21:58:19 +0800

    修改sql文件

--- src/main/mysql.sql
+++ src/main/mysql.sql
@@ -15,11 +15,11 @@ create table miaoshauser (
 
 create table goods (
   id bigint(20) not null auto_increment comment '商品ID',
-  name varchar(16) default null comment '商品名称',
-  title varchar(64) default null comment '商品标题',
-  img varchar(128) default null comment '商品图片',
+  goods_name varchar(16) default null comment '商品名称',
+  goods_title varchar(64) default null comment '商品标题',
+  goods_img varchar(128) default null comment '商品图片',
   detail longtext comment '商品的详情介绍',
-  price decimal(10, 2) default '0.00' comment '商品单价',
+  goods_price decimal(10, 2) default '0.00' comment '商品单价',
   stock int(11) default '0' comment '商品库存',
   primary key(id)
 ) engine=InnoDB auto_increment=3 charset=utf8mb4;
